a183a94d07d16bbec3e1356999440cf42d5758c5
junghun yang <luciferkala@gmail.com>
2021-02-05 00:26:03 +0900

    chnage to localhost

--- src/app.ts
+++ src/app.ts
@@ -26,7 +26,9 @@ const init = new InitController().excute()();
 // const kafkaInit = new KafkaController().excute()();
 app.use("/api", apiRouter);
 if (process.env.NODE_ENV !== "test")
-    app.listen(Number(process.env.SERVER_PORT) || 3000, "0.0.0.0");
+    app.listen(Number(process.env.SERVER_PORT) || 3000, "localhost");
 
-logger.info(`Server is running on 0.0.0.0 ${process.env.SERVER_PORT || 3000}!`);
+logger.info(
+    `Server is running on localhost ${process.env.SERVER_PORT || 3000}!`
+);
 export default app;
